66cf26e99c4767b3f633225135aa109814d2e756
remihuigen <remi@bureaubox.nl>
2025-02-19 22:03:38 +0100

    add readme
    * normalize manual input

---
+++ packages/directus-extension-slugify-operation/README.md
@@ -0,0 +1,20 @@
+# Slugify operation
+An operation for flows to slugify input values! This operation has been developed to be used directly upon `$trigger`. It takes input values, slugifies them, and adds an additional output field to the `$trigger.payload`
+
+
+## Installation
+Refer to the [Official Guide](https://docs.directus.io/extensions/installing-extensions.html) for details on installing the extension from the Marketplace or manually.
+
+## Configuration
+1. Set up a new flow with Event Hook trigger, filter (blocking), and choose which collections you want to slugify. You'll need to select the scope `items.create` and `items.update`, and set the response body to "Data of ;ast operation"
+2. Add the slugify operation. There you'll need to configure:
+   1. The input fields. Use their field key as value. If you select multiple input field, their values will al be used in the output slug
+   2. The output field key (for example: "slug"). This fieldkey will be added to the payload
+   3. Whether to make the slug unique by appending a random 6 character string at the end. 
+   4. Whether to lowercase the input values. Note that the random string CAN have capitals
+
+## Manual input 
+You can also manually set a slug. If the `$trigger.payload[output_key]` contains a value, the operation will do nothing, except forcing the provided value to be URL safe.
+
+That's it! Happy slugifying 😁
+
--- packages/directus-extension-slugify-operation/src/api.ts
+++ packages/directus-extension-slugify-operation/src/api.ts
@@ -17,24 +17,34 @@ function randomString(length: number): string {
 	return result;
 }
 
+const normalizeString = (str: string) => str.normalize('NFD')
+	.replace(/[\u0300-\u036f]/g, '')
+	.replace(/\s+/g, '-')  // Replace spaces with hyphens
+	.replace(/[^\w\s-]/g, '')  // Allow only alphanumeric, spaces, and hyphens
+	.replace(/[\s_-]+/g, '-')  // Convert spaces, underscores, and multiple hyphens to single hyphen
+	.replace(/^-+|-+$/g, '')   // Remove leading/trailing hyphens
+	.trim()
+
 export default defineOperationApi<Options>({
 	id: 'slugify',
 	handler: ({ fields, output_key, make_unique, lowercase }, { data }) => {
 		if (!data.$trigger || typeof data.$trigger !== 'object' || !('payload' in (data.$trigger as unknown as Object))) return {}
 
+		// If slug was manually provided, return that. But normalize it first.
+		if  ((data.$trigger as any).payload[output_key]) {
+			return { 
+				...(data.$trigger as any).payload, 
+				[output_key]: normalizeString((data.$trigger as any).payload[output_key])
+			};
+		}
+
 		let value = fields.map(field => (data.$trigger as any).payload[field]).filter(Boolean).join('-');
-		
-		// If input fields were not edited, or a slug was provided, return trigger data.
-		if (!value || (data.$trigger as any).payload[output_key]) return { ...(data.$trigger as any).payload };
+
+		// If input fields were not edited return trigger data.
+		if (!value) return { ...(data.$trigger as any).payload };
 
 		// Normalize accents using Unicode decomposition
-		const normalizedValue = value.normalize('NFD').replace(/[\u0300-\u036f]/g, '');
-
-		let slug = normalizedValue
-			.trim()
-			.replace(/[^\w\s-]/g, '')  // Allow only alphanumeric, spaces, and hyphens
-			.replace(/[\s_-]+/g, '-')  // Convert spaces, underscores, and multiple hyphens to single hyphen
-			.replace(/^-+|-+$/g, '');  // Remove leading/trailing hyphens
+		let slug = normalizeString(value);
 			
 		if (lowercase) {
 			slug = slug.toLowerCase();
