534a84946ad49e445c71af213c7f9a9c5edf82a0
remihuigen <remi@bureaubox.nl>
2025-02-28 15:00:44 +0100

    setup interface * add interface config * add interface options * fetch email value * add placeholder component

--- packages/directus-bundle-ms-exchange/src/email-interface/index.ts
+++ packages/directus-bundle-ms-exchange/src/email-interface/index.ts
@@ -2,11 +2,38 @@ import { defineInterface } from '@directus/extensions-sdk';
 import InterfaceComponent from './interface.vue';
 
 export default defineInterface({
-	id: 'custom',
-	name: 'Custom',
-	icon: 'box',
-	description: 'This is my custom interface!',
+	id: 'email_history_interface',
+	name: 'Email History',
+	icon: 'email',
+	description: 'List your organization email correspondence for a given email address.',
 	component: InterfaceComponent,
-	options: null,
-	types: ['string'],
+	options: ({collection}) => {
+		return [
+			{
+				field: 'email_field',
+				name: 'Email field',
+				type: 'string',
+				required: true,
+				meta: {
+					width: 'full',
+					required: true,
+					interface: "system-field",
+					options: {
+						collectionName: collection,
+						typeAllowList: ["string"],
+						allowNone: false,
+						multiple: false,
+						allowOther: false
+					},
+					note: 'Select the field that holds the email value for the collection'
+				},
+			}
+		]
+	},
+	hideLabel: true,
+	hideLoader: true,
+	autoKey: true,
+	group: 'presentation',
+	types: ['alias'],
+	localTypes: ['presentation'],
 });
--- packages/directus-bundle-ms-exchange/src/email-interface/interface.vue
+++ packages/directus-bundle-ms-exchange/src/email-interface/interface.vue
@@ -1,24 +1,65 @@
 <template>
-	<input :value="value" @input="handleChange($event.target.value)" />
+	<VNotice v-if="!isCreate" :type="type" :icon="pending ? 'autorenew' : undefined">
+		{{ label }}
+	</VNotice>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent, computed, ref, watch } from 'vue';
+import type { Ref } from 'vue';
+import { useApi } from '@directus/extensions-sdk';
 
 export default defineComponent({
 	props: {
-		value: {
+		email_field: {
 			type: String,
-			default: null,
+		},
+		primaryKey: {
+			type: [String, Number],
+			required: true
 		},
 	},
-	emits: ['input'],
-	setup(props, { emit }) {
-		return { handleChange };
+	setup(props,  { attrs }) {
+		const { collection } = attrs;
+		const api = useApi();
 
-		function handleChange(value: string): void {
-			emit('input', value);
+		const email = ref('')
+		const pending = ref(true)
+		const error: Ref<any> = ref(null)
+		async function fetchEmail() {
+			if (!props.email_field || isCreate.value) return;
+			pending.value = true
+			try {
+				const response = await api.get(`/items/${collection}/${props.primaryKey}`);
+				email.value = response.data.data[props.email_field];
+				pending.value = false
+				return ;
+			} catch (err) {
+				error.value = err;
+				console.warn(err);
+			}
 		}
+
+		const label = computed(() => {
+			if (!email.value) {
+				return 'No email address provided';
+			}
+			if (pending.value) return 'Searching for email history...';
+			return `Emails will be rendered here!`
+		})
+
+		const type = computed(() => {
+			if (!props.email_field) return 'warning'
+			return 'info'
+		})
+
+		const isCreate = computed(() => props.primaryKey === '+')
+
+
+		watch(() => props.primaryKey, fetchEmail)
+
+
+		return { pending, label, type, isCreate }
 	},
 });
 </script>
