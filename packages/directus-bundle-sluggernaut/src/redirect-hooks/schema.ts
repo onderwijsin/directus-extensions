0c0b4cbec3b9cf33834bf14cf397ba6605f9c7bc
remihuigen <remihuigen@gmail.com>
2025-03-10 20:38:23 +0100

    fix: Exclude sluggernaut changes

--- packages/directus-bundle-sluggernaut/src/redirect-hooks/schema.ts
+++ packages/directus-bundle-sluggernaut/src/redirect-hooks/schema.ts
@@ -431,6 +431,7 @@ export const settingsFieldSchema = [
 		"type": "alias",
 		"schema": null,
 		"meta": {
+			"id": 165,
 			"collection": "directus_settings",
 			"field": "divider_redirects",
 			"special": [
