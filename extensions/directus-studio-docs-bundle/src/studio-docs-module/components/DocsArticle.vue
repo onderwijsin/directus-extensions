c44c51a1c995c9655ad2c1de653e85fda673ffdd
Remi Huigen <remihuigen@proton.me>
2026-08-30 21:25:03 +0200

    chore: add css to markdown component

--- extensions/directus-studio-docs-bundle/src/studio-docs-module/components/DocsArticle.vue
+++ extensions/directus-studio-docs-bundle/src/studio-docs-module/components/DocsArticle.vue
@@ -1,36 +1,16 @@
 <script setup lang="ts">
 import type { StudioDocsArticle } from '../types'
 
+import 'github-markdown-css/github-markdown.css'
 import { Markdown } from '@comark/vue'
 
 defineProps<{ article: StudioDocsArticle }>()
 </script>
 
 <template>
-	<article class="article-layout">
-		<div class="article-content">
-			<Suspense>
-				<Markdown :value="article.body" />
-			</Suspense>
-		</div>
+	<article class="markdown-body">
+		<Suspense>
+			<Markdown :value="article.body" />
+		</Suspense>
 	</article>
 </template>
-
-<style scoped>
-.article-layout {
-	max-width: 1100px;
-	display: grid;
-	grid-template-columns: minmax(0, 1fr) 220px;
-	gap: 40px;
-}
-.article-content {
-	min-width: 0;
-	line-height: 1.65;
-}
-
-@media (max-width: 800px) {
-	.article-layout {
-		grid-template-columns: 1fr;
-	}
-}
-</style>
